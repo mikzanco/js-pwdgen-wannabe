c9059c2e2ff18cf436a91f078d0a9b8701001b61
mikzanco <mikyzancolo97@gmail.com>
2022-10-10 18:06:26 +0200

    fix colore anzichè anni

--- js/scripy.js
+++ js/scripy.js
@@ -1,10 +1,15 @@
 
 const nomeUtente = prompt('Come ti chiami?');
 const cognomeUtente = prompt('Qual è il tuo cognome?');
-const anniUtente = prompt('Quanti anni hai?');
+const colorUtente = prompt('Qual è il tuo colore preferito');
 
 
-output = nomeUtente + cognomeUtente + anniUtente;
+output = nomeUtente + cognomeUtente + colorUtente;
+
 
 
 document.getElementById('user').innerHTML = output;
+
+document.getElementById('firstName').innerHTML = nomeUtente;
+document.getElementById('lastName').innerHTML = cognomeUtente;
+document.getElementById('color').innerHTML = colorUtente;
